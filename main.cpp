9568f7459bed2e2175616926e56fea2e51f9ced5
Valentin Berthoz <valentin_berthoz@etu.u-bourgogne.fr>
2021-05-19 11:20:28 +0200

    Signed-off-by: Valentin Berthoz <valentin_berthoz@etu.u-bourgogne.fr>

--- main.cpp
+++ main.cpp
@@ -9,11 +9,6 @@ int main(int argc, char *argv[])
     w.show();
     return a.exec();
 
-    //Autre version de test entre PC et PC Hugo
-    //sECONDE PARTIE DU TEST
-    //derniere veersion du test
-    //encore un test
-    //mdr cette fois c'est la bonne
-    //issou c'est la bonne
+
 
 }
