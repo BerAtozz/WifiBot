f12723dbae2f64d05ece19265256a06d6dcee373
GEKO-SAN <husenechal@gmail.com>
2021-05-19 10:51:02 +0200

    update main

--- main.cpp
+++ main.cpp
@@ -8,9 +8,7 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
     return a.exec();
-<<<<<<< HEAD
-=======
 
-    //test3
->>>>>>> d73406afec4b30820aabdea4d6c1a0162b678e7c
+    //test4
+
 }
